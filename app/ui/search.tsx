697a47ec49c2437b6f74aec6cd5ca3057cd95f53
Michael Smolenski <>
2024-01-12 12:39:47 +0800

    Add debouncing to search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,6 +3,7 @@
 import {useState, useEffect} from 'react';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const [isClient, setIsClient] = useState(false);
@@ -14,7 +15,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams);
     if (term) {
       params.set('query', term);
@@ -22,7 +23,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
  
 
   if (!isClient) return <div />; // Fix for Lastpass extension that causes hydration error!
